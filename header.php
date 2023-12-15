622b0bc103e097113efd46850876edc70062ba4f
LeMustelide <marcvirgili.contact@gmail.com>
2023-12-15 23:11:06 +0100

    Update global.css and header.php

--- header.php
+++ header.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Master M1 MIAGE - Prédiction</title>
-    <link rel="stylesheet" href="./global.css">
+    <link rel="stylesheet" href="./global2.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 </head>
 <body>
     <div class="navbar">
-        <div class="title">M1 MIAGE</div>
+        <!-- <div class="title">M1 MIAGE</div> -->
+        <img src="https://cdn.vortexdev.fr/miagenda/long.png" alt="Logo MIAGE" height="24" width="192">
     </div>
     <div class="container">
